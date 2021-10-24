31f7e749296f6d1288b63996de6e99b66a0de7c1
fuoco <pedrofuoco6@gmail.com>
2021-10-24 20:58:52 -0300

    draft for simulation environment

---
+++ installers/gitman.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "$0: installing Gitman"
+
+sudo apt-get -y install python3-pip python3-setuptools
+sudo pip3 install gitman
+sudo -H pip3 install gitman
---
+++ installers/ros.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "$0: Installing ROS"
+
+# add repository for ignition library
+sudo apt-get -y install wget lsb-release gnupg
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get -y update
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-get -y install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt-get -y update
+
+sudo apt-get -y install ros-noetic-desktop-full
+
+num=`cat ~/.bashrc | grep "/opt/ros/$ROS_DISTRO/setup.bash" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  # set bashrc
+  echo "
+source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+
+fi
