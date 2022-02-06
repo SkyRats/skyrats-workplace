16ef3748f53ccf1c39339fef338a95e7530a6917
RicardoCLake <ricardodolago@gmail.com>
2022-02-05 22:52:13 -0300

    Default commit

--- installers2/ros2_ws.sh
+++ installers2/ros2_ws.sh
@@ -3,9 +3,41 @@ echo "$0: preparing skyrats_ws"
 
 # TODO: install colcon e create workspace
 # https://docs.ros.org/en/galactic/Tutorials/Workspace/Creating-A-Workspace.html
+# https://docs.ros.org/en/galactic/Tutorials/Colcon-Tutorial.html
+sudo apt install python3-colcon-common-extensions
+mkdir -p ~/skyrats_ws/src
+cd ~/skyrats_ws
+colcon build --symlink-install
 
-## Add workspace's setup.bash in bash.rc 
-COMMAND2="source ~/skyrats_ws/devel/setup.bash"
+
+## Add colcon_cd dependecies is .bashrc 
+COMMAND="source /usr/share/colcon_cd/function/colcon_cd.sh"
+num=`cat ~/.bashrc | grep "$COMMAND" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding '$COMMAND' to your .bashrc"
+  echo "$COMMAND" >> ~/.bashrc
+
+fi
+COMMAND="export _colcon_cd_root=/opt/ros/galactic/"
+num=`cat ~/.bashrc | grep "$COMMAND" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding '$COMMAND' to your .bashrc"
+  echo "$COMMAND" >> ~/.bashrc
+
+fi
+COMMAND="source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash"
+num=`cat ~/.bashrc | grep "$COMMAND" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding '$COMMAND' to your .bashrc"
+  echo "$COMMAND" >> ~/.bashrc
+
+fi
+
+## Add workspace's setup.bash in .bashrc 
+COMMAND2="source ~/skyrats_ws/install/setup.bash"
 num=`cat ~/.bashrc | grep "$COMMAND2" | wc -l`
 if [ "$num" -lt "1" ]; then
 
