175b59be62ed520c1117cc91d548125f5841199a
RicardoCLake <ricardodolago@gmail.com>
2022-02-10 13:00:03 -0300

    Comments changed

--- installers2/gitman.sh
+++ installers2/gitman.sh
@@ -5,7 +5,8 @@ sudo apt-get -y install python3-pip python3-setuptools
 sudo pip3 install gitman
 sudo -H pip3 install gitman
 
-#TODO: study the necessity of command repetetion 
+#TODO: study the necessity of command repetition 
 
-## Install gitman submodules 
+## Install gitman submodules
+## for exemp0lo, px4
 gitman install --force
