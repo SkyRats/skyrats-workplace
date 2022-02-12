d8a27f1ffe3f3ecf641e056946d632578057eea8
RicardoCLake <ricardodolago@gmail.com>
2022-02-12 00:47:07 -0300

    -y adds

--- installers2/ros2.sh
+++ installers2/ros2.sh
@@ -15,18 +15,18 @@ if [ $SUBLOCALE != "UTF-8"  ]; then
 fi
 
 ## Add universe repository
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository universe
 
 ## Add ROS repository
 sudo apt update
-sudo apt install curl gnupg lsb-release
+sudo apt install -y curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 ## Install ROS
 sudo apt update
-sudo apt install ros-galactic-desktop
+sudo apt install -y ros-galactic-desktop
 
 ## Sourcing the setup script
 source /opt/ros/galactic/setup.bash
@@ -43,6 +43,6 @@ if [ "$num" -lt "1" ]; then
 fi
 
 ## Install RQT
-sudo apt install ~nros-galactic-rqt*
+sudo apt install -y ~nros-galactic-rqt*
 
 ## TODO: tlvz precise instalar o Gazebo9 ou +
