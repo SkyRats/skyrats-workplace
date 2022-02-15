c7f2b9e58e5a2ed934da5bc766fcec695bf3a36b
RicardoCLake <ricardodolago@gmail.com>
2022-02-15 12:50:47 -0300

    Gitman problems fixed without flag --depth

--- installers2/gitman2.sh
+++ installers2/gitman2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "$0: installing Gitman"
 
+## Install gitman and its dependencies
+echo "$0: installing Gitman and its dependencies"
 sudo apt-get -y install python3-pip python3-setuptools
 sudo pip3 install gitman
 sudo -H pip3 install gitman
@@ -8,4 +9,5 @@ sudo -H pip3 install gitman
 ## Install gitman submodules
 # (PX4, Mavlink and Mavros)
 # (we won't use mavros with ros2)
+echo "$0: installing Gitman submodules"
 gitman install --force
