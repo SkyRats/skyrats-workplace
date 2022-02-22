f2f77549bab55bfc88ee73486efbe5e2cbe5864c
RicardoCLake <ricardodolago@gmail.com>
2022-02-22 10:23:44 -0300

    skyrats.sh updates

--- installers/skyrats.sh
+++ installers/skyrats.sh
@@ -26,37 +26,37 @@ sudo apt-get -y install git
 
 ## | ----------------------- install ROS2 ---------------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (1) \n\n"
 bash $MY_PATH/ros.sh
 
 ## | --------------------- install gitman --------------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (2) \n\n"
 bash $MY_PATH/gitman.sh 
 
 ## | ------------------- install fastRTPS --------------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (3) \n\n"
 bash $MY_PATH/fastRTPS.sh --no-gitman.sh
 
 ## | ----------------------- create skyrats_ws ---------------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (4) \n\n"
 bash $MY_PATH/ros_ws.sh
 
 ## | ---------------- install px4 dependencies ---------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (5) \n\n"
 bash $MY_PATH/px4.sh --no-gitman.sh
 
 ## | ---------------- install qgroundcontrol ---------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (6) \n\n"
 bash $MY_PATH/qgroundcontrol.sh
 
 ## | -------------- install others dependencies -------------- |
 
-echo -e "\n\n ... \n\n"
+echo -e "\n\n ... (7) \n\n"
 bash $MY_PATH/others.sh
 
 
