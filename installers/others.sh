c4db51ab9cd385f47e25ad49d36117a46393108b
GabiYuri <gabriela.ishikawa@gmail.com>
2022-03-03 09:40:21 -0300

    cleaning

--- installers/others.sh
+++ installers/others.sh
@@ -1,22 +1,5 @@
 echo "$0: installing general dependencies"
 
-### Generic tools
-#sudo apt-get -y install \
-#  #libeigen3-dev\ (deve estar em ros-galactic-eigen3-cmake-module)
-#  libopencv-dev\
-#  #libgeographic-dev\ (mavros)
-#  #geographiclib-tools\ (mavros)
-#  cmake\
-#
-### Phyton3 packages
-#sudo apt-get -y install \
-#  python3-setuptools\
-#  #python3-catkin-tools\
-#  python3-pip\
-#  #python3-future\ (mavlink)
-#  #python3-crcmod\ 
-#  #python3-lxml\ (mavlink)
-
 ## TODO:Perguntar para a gabs:
 #sudo apt install ros-galactic-eigen3-cmake-module
 #sudo pip3 install -U empy pyros-genmsg setuptools
