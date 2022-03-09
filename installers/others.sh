23ced375972abb5ab948c05964dd2902fc2d5e13
GabiYuri <gabriela.ishikawa@gmail.com>
2022-03-09 17:55:07 -0300

    remove chrome

--- installers/others.sh
+++ installers/others.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt -y install code
 
 ## TODO: Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt install -y google-chrome-stable
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#sudo apt-get update
+#sudo apt install -y google-chrome-stable
