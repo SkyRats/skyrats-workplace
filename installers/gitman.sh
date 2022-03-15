e7488646506498d1819f502161f28dbfe89ada91
pedro-fuoco <pedrofuoco6@gmail.com>
2022-03-15 10:44:35 -0300

    cleaning and organizing

--- installers/gitman.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$0: installing Gitman"
-
-sudo apt-get -y install python3-pip python3-setuptools
-sudo pip3 install gitman
-sudo -H pip3 install gitman
--- installers/qgroundcontrol.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo usermod -a -G dialout $USER
-sudo apt-get remove modemmanager -y
-sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
-cd ..
-wget https://s3-us-west-2.amazonaws.com/qgroundcontrol/latest/QGroundControl.AppImage
-chmod +x ./QGroundControl.AppImage
