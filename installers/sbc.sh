9cca12eb95b1318303ff269610af752fa74dd63c
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-15 13:38:34 -0300

    sbc 3

--- installers/sbc.sh
+++ installers/sbc.sh
@@ -1,5 +1,7 @@
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.sh
-./vcpkg integrate install
-./vcpkg install realsense2
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+# export http_proxy="http://<proxy>:<port>"
+# sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+
+# sudo apt-get install librealsense2-dkms
+# sudo apt-get install librealsense2-utils
+
