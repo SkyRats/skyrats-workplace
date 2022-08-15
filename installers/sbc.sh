cef79a25ac8fcb4c2cab974b108f9ffafe463ed9
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-15 13:19:08 -0300

    sbc 2

--- installers/sbc.sh
+++ installers/sbc.sh
@@ -1,9 +1,3 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
-
-sudo apt-get install librealsense2-dkms
-sudo apt-get install librealsense2-utils
-
-sudo apt-get install librealsense2-dev
-
-modinfo uvcvideo | grep "version:"
+sudo apt-get update
+git clone https://github.com/IntelRealSense/librealsense.git
+sudo apt-get install git libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at libudev-dev
