1dccc33f45f8b360a2c0507fa3117d83751ad494
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-15 13:02:55 -0300

    sbc help

---
+++ installers/sbc.sh
@@ -0,0 +1,9 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
+
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
+
+sudo apt-get install librealsense2-dev
+
+modinfo uvcvideo | grep "version:"
