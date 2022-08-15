ef9ef1f06497c8d89583617a3aeca138342317e5
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-15 13:23:03 -0300

    sbc 2

--- installers/sbc.sh
+++ installers/sbc.sh
@@ -1,3 +1,5 @@
-sudo apt-get update
-git clone https://github.com/IntelRealSense/librealsense.git
-sudo apt-get install git libssl-dev libusb-1.0-0-dev libudev-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev at libudev-dev
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+./vcpkg install realsense2
